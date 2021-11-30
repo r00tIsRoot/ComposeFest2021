03eaf7b5e2b833fb6126f9cfd1f720db663dbc43
rootBear <dlwodnjs0727@gmail.com>
2021-11-30 23:15:47 +0900

    [Codelab01]  - step12

--- week 1-Jetpack Compose basics/Compose01_Codelab01/app/src/main/java/com/isroot/compose01_codelab01/MainActivity.kt
+++ week 1-Jetpack Compose basics/Compose01_Codelab01/app/src/main/java/com/isroot/compose01_codelab01/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.isroot.compose01_codelab01
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.isroot.compose01_codelab01.ui.theme.Compose01_Codelab01Theme
@@ -70,7 +72,11 @@ private fun Greeting(name: String) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ))
             }
 
             OutlinedButton(
@@ -114,10 +120,17 @@ fun OnboardingPreview() {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     Compose01_Codelab01Theme {
-        MyApp()
+//        MyApp()
+        Greetings()
     }
 }
