11c8c71943d2bc06e255bfa4e06330e990d1613b
r00tIsRoot <35025164+r00tIsRoot@users.noreply.github.com>
2021-12-09 23:54:15 +0900

    - step 7

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopBarTest.kt
@@ -13,19 +13,6 @@ class TopAppBarTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun rallyTopAppBarTest() {
-        val allScreens = RallyScreen.values().toList()
-        composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = { },
-                currentScreen = RallyScreen.Accounts
-            )
-        }
-        Thread.sleep(5000)
-    }
-
-    @Test
     fun rallyTopAppBarTest_currentTabSelected() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
@@ -54,12 +41,34 @@ class TopAppBarTest {
 
         composeTestRule
             .onNode(
-                hasText(RallyScreen.Accounts.name.uppercase(Locale.getDefault())) and
+                hasText(RallyScreen.Accounts.name.uppercase()) and
                         hasParent(
                             hasContentDescription(RallyScreen.Accounts.name)
                         ),
                 useUnmergedTree = true
             )
             .assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_changeSelectedTab() {
+        val allScreen = RallyScreen.values().toList()
+        var currentTab: RallyScreen = RallyScreen.Overview
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreen,
+                onTabSelected = { screen ->
+                    currentTab = screen
+                },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        for (value in RallyScreen.values()) {
+            composeTestRule
+                .onNodeWithContentDescription(value.name)
+                .performClick()
+            assert(currentTab == value)
+        }
     }
 }
